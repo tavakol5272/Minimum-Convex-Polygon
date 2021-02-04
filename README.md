560cb65fa6980855e8342330ee7306e0e0496e4a
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-02-04 14:44:11 +0100

    first version files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Minimum-Convex-Polygon
+# Minimum Convex Polygon
+
+MoveApps
+
+Github repository: *github.com/andreakoelzsch/Minimum-Convex-Polygon*
+
+
+## Description
+Calculate the individual MCPs of the your Animals' locations and have them plotted on a colourful map. An additional small output file provides the MCP sizes.
+
+## Documentation
+After downsampling your data to maximum 5 minute resolution, this App calculates simple Minimum Convex Polygons (MCPs) for each individual of your data set. For calculating the planar shapes, the data set is first transformed to an equal area projection (+proj= +aeqd ...). The percentage of points that the MCP shall overlap can be defined by the user.
+
+The MCPs for each individual are then plotted on a OpenStreetMap of the area with transparent colours. On top, the downsampled locations are added in the same, individual specific colours. Finally, a csv-file with the area values of each MCP is added to the output for download.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+Shiny user interface (UI)
+
+### Artefacts
+`MCP_areas.csv`: csv-file with Table of all individuals and the sizes of their calculated MCPs. Note that this is done only once for the initial setting of `perc`. Unit of the area values: km^2.
+
+### Parameters 
+`perc`: Defined percentage of locations that the MCP algorithm shall use for calculating the MCP. We use the mcp() implementation of the adehabitat package, where (100 minus `perc` percent of the) locations furthest away from the centroid (arithmetric mean of the coordinates for each animal) are removed. Unit: `%` (range 0-100).
+
+### Null or error handling:
+**Parameter `perc`:** A default of 95 percent is provided and is retained if the input values is changed to NULL. If numbers above `100` are provided then the mcp function uses 100 percent of all locations. Negative values are not tolerated and will lead to an error.
+
+**Data:** The data are not manipulated in this App, but interactively explored. So that a possible Workflow can be continued after this App, the input data set is returned.
---
+++ ShinyModule.R
@@ -0,0 +1,74 @@
+library('move')
+library('ggmap')
+library('adehabitatHR')
+library('shiny')
+library('fields')
+library('scales')
+library('lubridate')
+
+shinyModuleUserInterface <- function(id, label, num, perc=95) {
+  ns <- NS(id)
+
+  tagList(
+    titlePanel("Minimum Convex Polygon(s) on a Map"),
+    sliderInput(inputId = ns("perc"), 
+                label = "Percentage of points the MCP should overlap", 
+                value = perc, min = 0, max = 100),
+    plotOutput(ns("map"),height="80vh")
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  
+  configuration <- list()
+  
+  configuration
+}
+
+shinyModule <- function(input, output, session, data, num=0.01, perc=95) {
+    current <- reactiveVal(data)
+
+    n.all <- length(timestamps(data))
+    data <- data[!duplicated(paste0(round_date(timestamps(data), "5 mins"), trackId(data))),]
+    logger.info(paste0("For better performance, the data have been thinned to max 5 minute resolution. From the total ",n.all," positions, the algorithm retained ",length(timestamps(data))," positions for calculation."))
+    
+    data.sp <- move2ade(data)
+    data.spt <- spTransform(data.sp,CRSobj=paste0("+proj=aeqd +lat_0=",round(mean(coordinates(data)[,2]),digits=1)," +lon_0=",round(mean(coordinates(data)[,1]),digits=1)," +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
+    
+    data.geo <- as.data.frame(data)[,c("location_long","location_lat","local_identifier")]
+    data.geo$local_identifier <- gsub(" ",".",data.geo$local_identifier)
+    
+    mcp.data <- reactive({ mcp(data.spt,percent=input$perc,unin="m",unout="km2") })
+    mcpgeo.data <- reactive({ spTransform(mcp.data(),CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0")) })
+    id <- reactive({ mcp.data()$id  })
+    cols <- reactive({ tim.colors(length(id())) })
+      
+    map <- get_map(bbox(extent(data)+c(-num,num,-num,num)))
+    
+    mcpmap <- reactive({
+      out <- ggmap(map) +
+        geom_polygon(data=fortify(mcpgeo.data()),
+                     aes(long,lat,colour=id,fill=id),
+                     alpha=0.3) +
+        geom_point(data=data.geo,
+                   aes(x=location_long, y=location_lat, colour=factor(local_identifier),shape='.'),show.legend=FALSE) +
+        theme(legend.position=c(0.15,0.80)) +
+        labs(x="Longitude", y="Latitude") +
+        scale_fill_manual(name="Animal", values=cols(), breaks=id()) +
+        scale_colour_manual(name="Animal",values=cols(), breaks=id())
+      out
+    })
+    
+    mcp.data.df <- data.frame(mcp(data.spt,percent=perc,unin="m",unout="km2"))
+    mcp.data.df$area <- round(mcp.data.df$area,digits=3)
+    names(mcp.data.df)[2] <- "area (km2)"
+    write.csv(mcp.data.df,"MCP_areas.csv",row.names=FALSE)
+
+  output$map <- renderPlot({
+    mcpmap()
+  })
+  
+  return(reactive({ current() }))
+}
+
