e2d8ef15d4b45a3513b0347da3558e5f368d7f53
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-09-09 10:51:17 +0200

    fix shp download

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Calculate the individual MCPs of the your Animals' locations and have them plott
 ## Documentation
 After downsampling your data to maximum 5 minute resolution, this App calculates simple Minimum Convex Polygons (MCPs) for each individual of your data set. For calculating the planar shapes, the data set is first transformed to an equal area projection (+proj= +aeqd ...). The percentage of points that the MCP shall overlap can be defined by the user.
 
-The MCPs for each individual are then plotted on a OpenStreetMap of the area with transparent colours. Underneath, the downsampled locations are added in the same, individual specific colours. Finally, a csv-file with the area values of each MCP and the shapefiles of the MCP are added to the output for download.
+The MCPs for each individual are then plotted on a OpenStreetMap of the area with transparent colours. Underneath, the downsampled locations are added in the same, individual specific colours. A csv-file with the area values of each MCP is added to the output for download. When pressing the button "save shapefile to output" the presently calculated MCP is saved to your outputs.
 
 ### Input data
 moveStack in Movebank format
@@ -22,11 +22,13 @@ Shiny user interface (UI)
 ### Artefacts
 `MCP_areas.csv`: csv-file with Table of all individuals and the sizes of their calculated MCPs. Note that this is done only once for the initial setting of `perc`. Unit of the area values: km^2.
 
-`shapefile_output/`: the four shapefile files of the MCP polygons for input to a GIS software. The files are called `mcp.dbf`, `mcp.prj`, `mcp.shp` and `mcp.shx`. The files are only generated for the initial parameter settings.
+`shapefile_output/`: the four shapefile files of the MCP polygons for input to a GIS software. The files are called `mcp.dbf`, `mcp.prj`, `mcp.shp` and `mcp.shx`. The files are generated for the at the time selected parameter settings. When pressing the button several times, the files get overwritten.
 
 
 ### Parameters 
 `perc`: Defined percentage of locations that the MCP algorithm shall use for calculating the MCP. We use the mcp() implementation of the adehabitat package, where (100 minus `perc` percent of the) locations furthest away from the centroid (arithmetric mean of the coordinates for each animal) are removed. Unit: `%` (range 0-100).
+
+`num`: Edge area of map around bounding box of the locations for better visibility. Unit: degrees. Default is 0.001. Note that this parameter cannot be interactively changed in the UI, but only set in the Settings in the WFI.
 
 ### Null or error handling:
 **Parameter `perc`:** A default of 95 percent is provided and is retained if the input values is changed to NULL. If numbers above `100` are provided then the mcp function uses 100 percent of all locations. Negative values are not tolerated and will lead to an error.
--- ShinyModule.R
+++ ShinyModule.R
@@ -14,7 +14,8 @@ shinyModuleUserInterface <- function(id, label, num=0.001, perc=95) {
     sliderInput(inputId = ns("perc"), 
                 label = "Percentage of points the MCP should overlap", 
                 value = perc, min = 0, max = 100),
-    plotOutput(ns("map"),height="80vh")
+    plotOutput(ns("map"),height="80vh"),
+    actionButton(ns("act"),"Add shapefile to output")
   )
 }
 
@@ -27,44 +28,45 @@ shinyModuleConfiguration <- function(id, input) {
 }
 
 shinyModule <- function(input, output, session, data, num, perc) {
-    current <- reactiveVal(data)
+  current <- reactiveVal(data)
     
-    n.all <- length(timestamps(data))
-    data <- data[!duplicated(paste0(round_date(timestamps(data), "5 mins"), trackId(data))),]
-    logger.info(paste0("For better performance, the data have been thinned to max 5 minute resolution. From the total ",n.all," positions, the algorithm retained ",length(timestamps(data))," positions for calculation."))
+  n.all <- length(timestamps(data))
+  data <- data[!duplicated(paste0(round_date(timestamps(data), "5 mins"), trackId(data))),]
+  logger.info(paste0("For better performance, the data have been thinned to max 5 minute resolution. From the total ",n.all," positions, the algorithm retained ",length(timestamps(data))," positions for calculation."))
     
-    data.sp <- move2ade(data)
-    data.spt <- spTransform(data.sp,CRSobj=paste0("+proj=aeqd +lat_0=",round(mean(coordinates(data)[,2]),digits=1)," +lon_0=",round(mean(coordinates(data)[,1]),digits=1)," +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
+  data.sp <- move2ade(data)
+  data.spt <- spTransform(data.sp,CRSobj=paste0("+proj=aeqd +lat_0=",round(mean(coordinates(data)[,2]),digits=1)," +lon_0=",round(mean(coordinates(data)[,1]),digits=1)," +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
     
-    data.geo <- as.data.frame(data)[,c("location_long","location_lat","trackId")] #trackId is already a valid name (validNames()), so no need to adapt
+  data.geo <- as.data.frame(data)[,c("location_long","location_lat","trackId")] #trackId is already a valid name (validNames()), so no need to adapt
 
-    mcp.data <- reactive({ mcp(data.spt,percent=input$perc,unin="m",unout="km2") })
-    mcpgeo.data <- reactive({ spTransform(mcp.data(),CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0")) })
+  mcp.data <- reactive({ mcp(data.spt,percent=input$perc,unin="m",unout="km2") })
+  mcpgeo.data <- reactive({ spTransform(mcp.data(),CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0")) })
     
-    mcp.data.static <- mcp(data.spt,percent=perc,unin="m",unout="km2")
-    writeOGR(mcp.data.static,dsn=Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),layer="mcp",driver="ESRI Shapefile")
-    #writeOGR(mcp.data.static,dsn="C:/tmp",layer="mcp",driver="ESRI Shapefile")
+  map <- get_map(bbox(extent(data)+c(-num,num,-num,num)))
 
-    map <- get_map(bbox(extent(data)+c(-num,num,-num,num)))
-    
-    mcpmap <- reactive({
-      out <- ggmap(map) +
-        geom_point(data=data.geo, 
-                   aes(x=location_long, y=location_lat, col=trackId,shape='.'),show.legend=FALSE) +
-        geom_polygon(data=fortify(mcpgeo.data()),
+  mcpmap <- reactive({
+    out <- ggmap(map) +
+      geom_point(data=data.geo, 
+                 aes(x=location_long, y=location_lat, col=trackId,shape='.'),show.legend=FALSE) +
+       geom_polygon(data=fortify(mcpgeo.data()),
                      aes(long,lat,colour=id,fill=id),
                      alpha=0.3) +
-        theme(legend.position=c(0.15,0.80)) +
-        labs(x="Longitude", y="Latitude") +
-        scale_fill_manual(name="Animal", values=tim.colors(length(id)),aesthetics=c("colour","fill"))
-      out
-    })
+      theme(legend.position=c(0.15,0.80)) +
+      labs(x="Longitude", y="Latitude") +
+      scale_fill_manual(name="Animal", values=tim.colors(length(namesIndiv(data))),aesthetics=c("colour","fill"))
+    out
+  })
     
-    mcp.data.df <- data.frame(mcp(data.spt,percent=perc,unin="m",unout="km2"))
-    mcp.data.df$area <- round(mcp.data.df$area,digits=3)
-    names(mcp.data.df)[2] <- "area (km2)"
-    write.csv(mcp.data.df,paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"MCP_areas.csv"),row.names=FALSE)
+  mcp.data.df <- data.frame(mcp(data.spt,percent=perc,unin="m",unout="km2"))
+  mcp.data.df$area <- round(mcp.data.df$area,digits=3)
+  names(mcp.data.df)[2] <- "area (km2)"
+  write.csv(mcp.data.df,paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"MCP_areas.csv"),row.names=FALSE)
 
+  observeEvent(input$act, {
+    writeOGR(obj=mcpgeo.data(), dsn=Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), layer="mcp", 
+             driver="ESRI Shapefile", overwrite_layer=TRUE)
+  })
+    
   output$map <- renderPlot({
     mcpmap()
   })
