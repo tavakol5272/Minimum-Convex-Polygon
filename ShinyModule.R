7df8b9f63921ba370478d8701ef30c370dd5d575
tavakol5272 <148060212+tavakol5272@users.noreply.github.com>
2025-04-11 14:30:43 +0200

    Update ShinyModule.R

--- ShinyModule.R
+++ ShinyModule.R
@@ -39,7 +39,7 @@ shinyModuleUserInterface <- function(id, label) {
         downloadButton(ns("download_gpkg"), "Download MCP as GPKG", class = "btn-sm"),
         downloadButton(ns("download_mcp_table"), "Download MCP Table (CSV)", class = "btn-sm"),
         
-      
+        
         ,width = 4
       ),
       
@@ -55,27 +55,21 @@ shinyModuleUserInterface <- function(id, label) {
 #####server######
 
 shinyModule <- function(input, output, session, data) {
-  # all IDs of UI functions need to be wrapped in ns()
   ns <- session$ns
   current <- reactiveVal(data)
   dataObj <- reactive({ data })
   
- 
+  
   # exclude all individuals with less than 5 locations
   data_filtered <- reactive({
     req(data)
-    #filter_ids <- names(which(table(mt_track_id(data)) >= 5))  
-    #data[mt_track_id(data) %in% filter_ids, ]
     data %>%
       group_by(individual_name_deployment_id) %>%
       filter(n() >= 5) %>%
       ungroup()
-    
-    
-    #mt_as_move2(filtered, time_column = "timestamp", track_id_column = "individual_name_deployment_id")
   })
   
-  ##select desired animal among animal with more than 5 location
+  ##select animalin slide bar
   observe({
     req(data_filtered())
     animal_choices <- unique(mt_track_id(data_filtered()))
@@ -92,50 +86,41 @@ shinyModule <- function(input, output, session, data) {
     selected
   })
   
- 
   
   
-  # Compute the MCP for the filtered tracks
+  
+  # Compute the MCP 
   mcp_cal <- reactive({
     req(input$perc)
     data_sel <- selected_data()
     #print(class(data_sel))
+    
     move_obj <- mt_as_move2(
       data_sel,
       time_column = "timestamp",  
       track_id_column = "individual_name_deployment_id"  
     )
-    print(class(move_obj))
-    move_stack <- to_move(move_obj)
+    #print(class(move_obj))
     
+    move_stack <- to_move(move_obj)
+    #print(class(move_stack))
     
     sf_data <- st_as_sf(move2ade(move_stack))
-    #sf_data <- st_as_sf(move2ade(move_stack))
-    print(class(sf_data))
-    crs_proj <- mt_aeqd_crs(sf_data, center = "centroid", units = "m")
+    #print(class(sf_data))
+    
+    #crs_proj <- mt_aeqd_crs(sf_data, center = "centroid", units = "m")
     #sf_data_proj <- st_transform(sf_data, crs_proj)
+    
     sf_data_proj <- st_transform(sf_data, 4326)
-    print(class(sf_data_proj))
+    #print(class(sf_data_proj))
+    
     sp_data_proj <- as(sf_data_proj, "Spatial")
-    print(class(sp_data_proj))
+    #print(class(sp_data_proj))
+    
     sf_data_proj$id = sf_data$individual_name_deployment_id
     
-    data_mcp <- tryCatch({
-      adehabitatHR::mcp(sp_data_proj, percent = input$perc, unin = "m", unout = "km2")
-    }, error = function(e) {
-      warning("MCP computation failed: ", e)
-      return(NULL)
-    })
-    #data_mcp <- adehabitatHR::mcp(sp_data_proj, percent = input$perc, unin = "m", unout = "km2")
-    
-    print(class(data_mcp))
-    
-    if (!is.null(data_mcp)) {
-      if (any(data_mcp@data$area == 0)) {
-        warning("Some MCPs have zero area.")
-      }
-    }
-    
+    data_mcp <- adehabitatHR::mcp(sp_data_proj, percent = input$perc, unin = "m", unout = "km2")
+    #print(class(data_mcp))
     
     return(list(data_mcp = data_mcp,track_lines = mt_track_lines(data_sel)))
     
@@ -145,88 +130,59 @@ shinyModule <- function(input, output, session, data) {
   
   ##leaflet map####
   mmap <- reactive({
-      req(mcp_cal())
-      mcp <- mcp_cal()
-      bounds <- as.vector(st_bbox(dataObj()))
+    req(mcp_cal())
+    mcp <- mcp_cal()
+    print(mcp)
+    bounds <- as.vector(st_bbox(dataObj()))
+    
+    sf_mcp <- st_as_sf(mcp$data_mcp)
+    track_lines <- mcp$track_lines
+    ids <- unique(mcp$track_ids)
+    pal <- colorFactor(palette = pals::cols25(), domain = ids)
+    
+    leaflet(options = leafletOptions(minZoom = 2)) %>% 
+      fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%       
+      addTiles() %>%
+      addProviderTiles("Esri.WorldTopoMap", group = "TopoMap") %>%
+      addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
+      addTiles(group = "Default") %>%
+      addScaleBar(position = "topleft") %>%
       
-      # Safely handle if data_mcp is NULL
-      #if (is.null(mcp$data_mcp)) {
-        # Return an empty map or a map with just the base tiles
-        #return(leaflet() %>%
-                 #fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
-                 #addTiles())
-      #}
+      addPolylines(data = track_lines, color = ~pal(track_lines$individual_name_deployment_id),
+                   weight = 3, group = "Tracks") %>%
+      addPolygons(data = sf_mcp, fillColor = ~pal(rownames(sf_mcp)),color = "black",fillOpacity = 0.4,
+                  weight = 2,label = ~rownames(sf_mcp) ,group = "MCPs") %>%
       
-      # Safely handle cases where there are no valid polygons
-      #if (length(mcp$data_mcp) == 0) {
-        #return(leaflet() %>%
-                 #fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
-                 #addTiles())
-      #}
+      addLegend(position = "topleft",pal = pal,values = ids,title = "Animal") %>%
       
-      
-      
-      sf_mcp <- st_as_sf(mcp$data_mcp)
-      track_lines <- mcp$track_lines
-      #ids <- unique(mt_track_id(track_lines))
-      ids <- unique(mcp$track_ids)
-      pal <- colorFactor(palette = pals::cols25(), domain = ids)
-      
-      leaflet(options = leafletOptions(minZoom = 2)) %>% 
-          fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%       
-          addTiles() %>%
-          addProviderTiles("Esri.WorldTopoMap", group = "TopoMap") %>%
-          addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
-          addTiles(group = "Default") %>%
-          addScaleBar(position = "topleft") %>%
-          
-          #addPolylines(data = track_lines,color = ~pal(mt_track_id(track_lines)),weight = 2,group = "Tracks") %>%
-          addPolylines(data = track_lines, color = ~pal(track_lines$individual_name_deployment_id),
-                     weight = 3, group = "Tracks") %>%
-          addPolygons(data = sf_mcp, fillColor = ~pal(rownames(sf_mcp)),color = "black",fillOpacity = 0.4,
-          weight = 2,label = ~rownames(sf_mcp) ,group = "MCPs") %>%
-        
-          addLegend(position = "topleft",pal = pal,values = ids,title = "Animal") %>%
-        
-          addLayersControl(
-              baseGroups = c("Default", "TopoMap", "Aerial"),
-              overlayGroups = c("Tracks", "MCPs"),
-              options = layersControlOptions(collapsed = FALSE)
-          )
-     })
-                 
+      addLayersControl(
+        baseGroups = c("Default", "TopoMap", "Aerial"),
+        overlayGroups = c("Tracks", "MCPs"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  
   output$leafmap <- renderLeaflet({mmap()})
   
   ###download the table of mcp
   output$download_mcp_table <- downloadHandler(
-    filename = function() {
-      paste0("mcp_table_", Sys.Date(), ".csv")
-    },
+    filename = "mcp_table.csv",
     content = function(file) {
       mcp <- mcp_cal()$data_mcp
-      
-      # Check if mcp is NULL or has no rows
-      if (is.null(mcp) || length(mcp) == 0) {
-        # Create a data frame with a message
-        df <- data.frame(message = "No MCP data available.")
-      } else {
-        # Convert to data frame safely
-        mcp_df <- as.data.frame(mcp)
-        
-        # Prepare the data frame with TrackID and Area_km2
-        df <- data.frame(TrackID = rownames(mcp_df), Area_km2 = round(mcp_df$area, 3))
-      }
-      
+      mcp_df <- as.data.frame(mcp)
+      df <- data.frame(TrackID = rownames(mcp_df), Area = mcp_df$area)
       write.csv(df, file, row.names = FALSE)
     }
   )
-
+  
+  
+  
   ### save map as HTML
   output$save_html <- downloadHandler(
     filename = "LeafletMap.html",
     content = function(file) {
       saveWidget(widget = mmap(),file=file) })
-  
+
   ### save map as PNG
   output$save_png <- downloadHandler(
     filename = "LeafletMap.png",
@@ -234,7 +190,6 @@ shinyModule <- function(input, output, session, data) {
       temp_html <- tempfile(fileext = ".html")
       saveWidget(mmap(), file = temp_html, selfcontained = TRUE)
       webshot2::webshot(url = temp_html, file = file, vwidth = 1000, vheight = 800) })
-    
   
   ###download shape as kmz  
   output$download_kmz <- downloadHandler(
@@ -262,6 +217,3 @@ shinyModule <- function(input, output, session, data) {
   
   return(reactive({ current() }))
 }
-
-
-
